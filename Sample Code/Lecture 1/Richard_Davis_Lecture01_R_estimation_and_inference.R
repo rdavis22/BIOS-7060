9b2694afef2a70f9c15fddd35993b70a969af3c6
Richard Davis <rdavis22@tulane.edu>
2017-01-21 20:12:49 +0000

    GGplot used to replace everything up to the 'matplot'; need more work on the matplot

--- Sample Code/Lecture 1/Richard_Davis_Lecture01_R_estimation_and_inference.R
+++ Sample Code/Lecture 1/Richard_Davis_Lecture01_R_estimation_and_inference.R
@@ -1,7 +1,11 @@
 ####Example codes for running a linear regression analysis####
 ##libraries
 if(!require(tidyverse))
-  install.packages(tidyverse)
+  install.packages("tidyverse")
+if(!require(cowplot))
+  install.packages("cowplot")
+if(!require(reshape2))
+  install.packages("reshape2")
 
 
 # Read in the country-savings data set
@@ -10,7 +14,7 @@ savings  <- read.table("Lecture01_data_estimation_and_inference.txt", header=T)
 # Summary statistics of the data
 summary(savings)
 
-# Two plots side by side: histogram and kernel density for personal savings
+## Two plots side by side: histogram and kernel density for personal savings
 #GGplot
 p1<-ggplot(data=savings, aes(savings$sr))+
   geom_histogram()
@@ -19,20 +23,27 @@ p2<-ggplot(data=savings, aes(savings$sr))+
 # hist(savings$sr) # Histogram
 # plot(density(savings$sr)) # Kernel density plot 
 
-# Put kernel density on the histogram
-hist(savings$sr, prob=T) # Histogram
-lines(density(savings$sr), col="red") # Kernel density plot 
+## GGplot: Put the two graphs on the same plot
+#'plot_grid' function is from "cowplot" package
+plot_grid(p1, p2, labels=c("Hist", "Density"), ncol=2, nrow=1)
+# hist(savings$sr, prob=T) # Histogram
+# lines(density(savings$sr), col="red") # Kernel density plot 
 
-# Plot personal savings (sr) vs percentage population under 15 yrs (pop15)
-plot(x=savings$pop15, y=savings$sr, cex=1.5, pch=16, col="red", cex.lab=2,
-	cex.axis=1.5, xlab="Percentage population <15 yrs",
-	ylab="Personal savings")
+## GGplot: personal savings (sr) vs percentage population under 15 yrs (pop15)
+p_sr_pop15<-ggplot(data=savings, aes(x=sr, y=pop15))+
+  geom_point(color="red")+
+  labs(x="Percentage population <15 yrs", y="Personal Savings")
+
+# plot(x=savings$pop15, y=savings$sr, cex=1.5, pch=16, col="red", cex.lab=2,
+# 	cex.axis=1.5, xlab="Percentage population <15 yrs",
+# 	ylab="Personal savings")
 
 # simple linear regression: y - personal savings, x - pop15
 g1 <- lm(sr~pop15, data=savings)
 
 # trend line for the regression
-abline(g1$coef, lty=2, lwd=2, col="blue")
+p_sr_pop15<-p_sr_pop15+geom_smooth(method="lm", se=FALSE)
+# abline(g1$coef, lty=2, lwd=2, col="blue")
 
 # Summary of regression results: parameter estimation & sd, t-test for 
 #   individual predictor, R2, F-test for the model
@@ -41,10 +52,12 @@ summary(g1)
 # ANOVA table for regression
 anova(g1)
 
-# Residual plot
-plot(x=g1$fitted.values, y=g1$residuals, cex=1.5, pch=16, col="red", cex.lab=2,
-	cex.axis=1.5, xlab="Fitted values",
-	ylab="Residuals")
+#GGPlot: Residual plot
+p_res<-qplot(x=g1$fitted.values, y=g1$residuals, geom="point",
+             xlab="Fitted Values", ylab="Residuals", colour=I("red"))
+# plot(x=g1$fitted.values, y=g1$residuals, cex=1.5, pch=16, col="red", cex.lab=2,
+# 	cex.axis=1.5, xlab="Fitted values",
+# 	ylab="Residuals")
 
 
 # 95% confidence interval for parameters 
@@ -55,9 +68,11 @@ confint(g1)
 tt = order(savings$pop15)
 conf_lim = predict(g1, interval="confidence")
 pred_lim = predict(g1, interval="prediction")
-matplot(savings$pop15[tt], cbind(conf_lim, pred_lim[,-1])[tt,], type="l", 
-	ylab="predicted sr", xlab="Pop15", lty=c(1, 2, 2, 3, 3),
-	col=c(1, 2, 2, 3, 3))
+
+
+# matplot(savings$pop15[tt], cbind(conf_lim, pred_lim[,-1])[tt,], type="l", 
+# 	ylab="predicted sr", xlab="Pop15", lty=c(1, 2, 2, 3, 3),
+# 	col=c(1, 2, 2, 3, 3))
 
 # for powerpoint files
 hist(savings$sr, cex.lab=2, cex.axis=2, main="", breaks=20, xlab="Personal savings", prob=T, col="grey")
