96bfc5b747797e7b0672919c746d7a66bf4bc31b
Richard Davis <rdavis22@tulane.edu>
2017-02-05 05:43:17 +0000

    Problem 1 done

--- Homework/Hw 1/BIOS_7060_Hw1.R
+++ Homework/Hw 1/BIOS_7060_Hw1.R
@@ -11,9 +11,38 @@ if(!require(MethComp))
 
 #Diabetes Data
 #hw1.data<-read.table(file=file.choose(), header=T, sep="")
-attach(hw1.data)
 
 ####Problem 1####
-##Part A##
-#fit hba1c vs. fbg
-hw1a.model<-lm(hba1c~fbg, data=hw1.data)
+###Part A###
+#subset data to have just diabetes patients
+hw1a.data<-subset(hw1.data, hba1c>6.5)
+##1A-1) fit hba1c vs. fbg
+hw1a.model<-lm(hba1c~fbg, data=hw1a.data)
+
+##1A-2) pure error (l-o-f) and homogeneity
+#*Pure Error: Lack-of-Fit
+#choose 5 categories from which to subset the diabetes data
+hw1a2.data<-subset(hw1a.data, fbg_round==5.2|fbg_round==7.2|fbg_round==9.2|
+                     fbg_round==11.2|fbg_round==12.8)
+#get the model to determine the pure Error
+hw1a2.model<-lm(hba1c~fbg_round, data=hw1a2.data)
+
+#pure error from the alr3 package
+purErr<-pureErrorAnova(hw1a2.model)
+
+#alternate method for pure error 
+hw1a2_fac.model<-lm(hba1c~factor(fbg_round), data=hw1a2.data)
+purErr_alt<-anova(hw1a2.model, hw1a2_fac.model)
+
+#*Homogeneity of pure errors
+#will return a logical vector
+t01<-apply(outer(hw1a2.data$fbg_round, hw1a2.data$fbg_round, "=="), 1, sum)>1
+#subset data based on t0
+tmpD<-hw1a2.data[t01,]
+
+#Levene's test: mean
+lvne_mn.prb1<-leveneTest(tmpD$hba1c, tmpD$fbg_round, center = mean)
+#Levene's test: median
+lvne_md.prb1<-leveneTest(tmpD$hba1c, tmpD$fbg_round, center = median)
+#Bartlest's test
+brtlt.prb1<-bartlett.test(tmpD$hba1c, tmpD$fbg_round)
