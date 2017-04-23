c368578257f2d8deaff04fb49d74e8ef2c1873e4
Richard Davis <rdavis22@tulane.edu>
2017-04-23 05:43:31 +0100

    Problem 3 done

--- Homework/Hw 3/BIOS_7060_Hw3.R
+++ Homework/Hw 3/BIOS_7060_Hw3.R
@@ -6,10 +6,14 @@ if(!require(car)){
   install.packages("car"); library(car)}
 if(!require(MASS)){
   install.packages("MASS"); library(MASS)}
-
-####Problem 1####
-###Data Input###
+if(!require(faraway)){
+  install.packages("faraway"); library(faraway)}
+###Set the proper file path###
 #path<-"C:/Users/Rick/Documents/Tulane/MPH/BIOS\ 7060/Homework/Hw\ 3/"
+#***Make sure to set the path as the one with 'Hw3_1.txt' in it.
+
+####Problem 3.1####
+###Data Input###
 prb3_1.data<-read.table(file="Hw3_1.txt", header=T)
 #convert dataframe to more useable tibble forr from dplyr
 prb3_1.tibble<-as_tibble(prb3_1.data)
@@ -57,16 +61,11 @@ prb3_2.BIC<-step(prb3_2.lm, direction = "both", k=log(nrow(prb3_2.tibble)))
 attach(prb3_1.tibble)
 prb3_3.tibble<-tibble(sex, hbtrans.prb3_1, fbg, hba1c)
 detach(prb3_1.tibble)
+#attach the new data frame to use ggplot2 without having to specify the dataframe...
+#...in the 'ggplot' statement
+attach(prb3_3.tibble)
 
-#regression in males vs. female
-prb3_3male.lm<-lm(hbtrans.prb3_1[sex=="male"]~fbg[sex=="male"], data = prb3_3.tibble)
-prb3_3female.lm<-lm(hbtrans.prb3_1[sex=="female"]~fbg[sex=="female"], data = prb3_3.tibble)
-
-#regression in diabetics vs. non-diabetics
-prb3_3dia.lm<-lm(hbtrans.prb3_1[hba1c>6.5]~fbg[hba1c>6.5], data = prb3_3.tibble)
-prb3_3nondia.lm<-lm(hbtrans.prb3_1[hba1c<6.5]~fbg[hba1c<6.5], data = prb3_3.tibble)
-
-##Plots for the regression lines##
+###Plots for the regression lines (visualized before running analysis###
 #Overall relationship
 prb3_3overall.ggplot<-ggplot(data = prb3_3.tibble)+
   geom_jitter(aes(x=fbg, y=hbtrans.prb3_1))+
@@ -81,10 +80,93 @@ prb3_3sex.ggplot<-ggplot()+
   geom_smooth(aes(x=fbg[sex=="female"], y=hbtrans.prb3_1[sex=="female"]), method = lm, se=T, colour="red")+
   labs(x = "fbg", y = "Transformed hba1c", title="Transformed hba1c vs. fbg in Males (blue) and Females (red)")
 
-#
-prb3_3sex.ggplot<-ggplot()+
-  geom_jitter(aes(x=fbg[sex=="male"], y=hbtrans.prb3_1[sex=="male"]), colour="blue")+
-  geom_smooth(aes(x=fbg[sex=="male"], y=hbtrans.prb3_1[sex=="male"]), method = lm, se=F, colour="blue")+
-  geom_jitter(aes(x=fbg[sex=="female"], y=hbtrans.prb3_1[sex=="female"]), colour="red")+
-  geom_smooth(aes(x=fbg[sex=="female"], y=hbtrans.prb3_1[sex=="female"]), method = lm, se=F, colour="red")+
-  labs(x = "fbg", y = "Transformed hba1c", title="Transformed hba1c vs. fbg in Males (blue) and Females (red)")
+#Diabetics vs. non-diabetics
+prb3_3dia.ggplot<-ggplot()+
+  geom_jitter(aes(x=fbg[hba1c<6.5], y=hbtrans.prb3_1[hba1c<6.5]), colour="blue")+
+  geom_smooth(aes(x=fbg[hba1c<6.5], y=hbtrans.prb3_1[hba1c<6.5]), method = lm, se=T, colour="blue")+
+  geom_jitter(aes(x=fbg[hba1c>6.5], y=hbtrans.prb3_1[hba1c>6.5]), colour="red")+
+  geom_smooth(aes(x=fbg[hba1c>6.5], y=hbtrans.prb3_1[hba1c>6.5]), method = lm, se=T, colour="red")+
+  labs(x = "fbg", y = "Transformed hba1c", title="Transformed hba1c vs. fbg in Non-diabetics (blue) and Diabetics (red)")
+
+#detach and then explicitly call variables in the 'prb3_3.tibble' dataframe to avoid...
+#...confusion with dataframes from problems 1 and 2
+detach(prb3_3.tibble)
+
+###regression in males vs. female (dummy code)###
+#initiliaze dummy-coded fasting blood glucose variable
+fac_fbg<-c()
+for (i in 1:length(prb3_3.tibble$hbtrans.prb3_1)){
+  if (prb3_3.tibble$sex[i]=="male"){
+    #males will be coded as "0"
+    fac_fbg[i]<-0
+  }
+  else if (prb3_3.tibble$sex[i]=="female"){
+    #females will be coded as "1"
+    fac_fbg[i]<-1
+  }
+}
+#make class 'fac_fbg' class "factor" and then make a part of the tibble
+fac_fbg<-factor(fac_fbg)
+prb3_3.tibble<-add_column(prb3_3.tibble, fac_fbg)
+
+##regression equations##
+#dummy-coded regression for males(=0) vs. females(=1)
+prb3_3sex.lm<-lm(hbtrans.prb3_1~fac_fbg, data = prb3_3.tibble)
+#regression for just males
+prb3_3male.lm<-lm(hbtrans.prb3_1[sex=="male"]~fbg[sex=="male"], data = prb3_3.tibble)
+#regression for just females
+prb3_3female.lm<-lm(hbtrans.prb3_1[sex=="female"]~fbg[sex=="female"], data = prb3_3.tibble)
+
+###regression in diabetics vs. non-diabetics###
+#create categorical outcome variable
+fac_hbtrans<-c()
+for (i in 1:length(prb3_3.tibble$hbtrans.prb3_1)){
+  if (prb3_3.tibble$hba1c[i]<6.5){
+    #non-diabetics will be coded as 0
+    fac_hbtrans[i]<-0
+  }
+  else if (prb3_3.tibble$hba1c[i]>6.5){
+    #diabetics will be coded as 1
+    fac_hbtrans[i]<-1
+  }
+}
+#make class 'fac_hbtrans' class "factor" and then make a part of the tibble
+fac_hbtrans<-factor(fac_hbtrans)
+prb3_3.tibble<-add_column(prb3_3.tibble, fac_hbtrans)
+##regression equations##
+#logistic regression for diabetic vs. non-diabetic outcome
+prb3_3dia.glm<-glm(fac_hbtrans~fbg, family=binomial, data = prb3_3.tibble)
+
+#plot with diabetics and non-diabetics classified as categorical
+prb3_3diacat.ggplot<-ggplot()+
+  geom_jitter(aes(x=fbg[fac_hbtrans==0], y=fac_hbtrans[fac_hbtrans==0]), colour="blue")+
+  geom_smooth(aes(x=fbg[fac_hbtrans==0], y=fac_hbtrans[fac_hbtrans==0]), method = lm, se=T, colour="blue")+
+  geom_jitter(aes(x=fbg[fac_hbtrans==1], y=fac_hbtrans[fac_hbtrans==1]), colour="red")+
+  geom_smooth(aes(x=fbg[fac_hbtrans==1], y=fac_hbtrans[fac_hbtrans==1]), method = lm, se=T, colour="red")+
+  labs(x = "fbg", y = "transformed hba1c", title="Transformed hba1c vs. fbg in Non-diabetics(=0, blue) and Diabetics(=1,red)")
+  
+# prb3_3dia.lm<-lm(hbtrans.prb3_1[hba1c>6.5]~fbg[hba1c>6.5], data = prb3_3.tibble)
+# prb3_3nondia.lm<-lm(hbtrans.prb3_1[hba1c<6.5]~fbg[hba1c<6.5], data = prb3_3.tibble)
+
+
+####Problem 3.4####
+###Data Input###
+prb3_4.data<-read.table(file="Hw3_2.txt", header=T)
+#convert dataframe to more useable tibble forr from dplyr
+prb3_4.tibble<-as_tibble(prb3_4.data)
+#remove dataframe
+rm(prb3_4.data)
+attach(prb3_4.tibble)
+
+###scaling and centering Data###
+#intial regression model
+g0<-lm(CHOL ~ ., data = prb3_4.tibble)
+#new dataframe with ID, PILL, and PAIR
+prb3_4_0.tibble<-tibble(AGE, HT, WT, CHOL, ALB, CALC, URIC, wtalb, acucont)
+#scaled new dataframe
+prb3_4_0sc.tibble<-as_tibble(data.frame(scale(prb3_4_0.tibble)), na.rm=T)
+#remove missing values
+prb3_4_0sc.tibble<-na.omit(prb3_4_0sc.tibble)
+
+##check the correlation##
+prb3_4.cor<-round(cor(prb3_4_0sc.tibble, use = "complete.obs"), 3)
